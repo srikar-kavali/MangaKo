525552244073955d53b706c4a775c5fe1291950b
Srikar <srikar.kavali@gmail.com>
2025-07-03 20:31:45 -0500

    Learned Text, Image, View, and Stylesheet. Uploaded first image onto app.

---
+++ app/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button, Image} from "react-native";
+import TennisBall from "../assets/tennisBall.png"
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={TennisBall}
+                style={styles.image}
+            />
+
+            <Text style={[styles.title, {color : 'darkgreen'}]}>Hello World</Text>
+            <Text style={{marginTop : 10, marginBottom : 30}}>
+                First App
+            </Text>
+
+        </View>
+
+
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+
+        // iOS
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+
+        // Android
+        elevation: 4,
+    },
+    image: {
+        marginVertical: 20,
+        width: 100,
+        height: 100,
+        resizeMode: 'cover',
+    }
+});
