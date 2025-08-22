4bc1eb96e99a19f1f133488d7240549c2b4e7559
Srikar Kavali <srikar.kavali@gmail.com>
2025-08-22 18:24:40 -0500

    chore: simplify ping endpoint

--- api/ping.py
+++ api/ping.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def ping():
     return {"ok": True, "service": "mangapill"}
-
-# Vercel handler
-def handler(request):
-    from mangum import Mangum
-    asgi_handler = Mangum(app)
-    return asgi_handler(request.scope, request.receive, request.send)
