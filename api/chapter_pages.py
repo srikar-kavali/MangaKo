3b4f32cb1b5fd677680b5ba72e693fc8bad60917
Srikar <srikar.kavali@gmail.com>
2025-11-02 17:53:16 -0600

    Testing

--- api/chapter_pages.py
+++ api/chapter_pages.py
@@ -1,3 +1,4 @@
+#api
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- api/image_proxy.py
+++ api/image_proxy.py
@@ -1,4 +1,3 @@
-# api/image_proxy.py
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
