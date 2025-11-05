cf525b539723b985b8719f0939e68d7835003712
Srikar <srikar.kavali@gmail.com>
2025-11-04 20:08:48 -0600

    Fixing cors issue for web version.

--- api/search.py
+++ api/search.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI, HTTPException, Query
 from scrapers.mangapill_scraper import MangapillScraper
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Mangapill Search", root_path="/api/search")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8081",
-        "http://localhost:3000",
-        "https://manga-8jrq7kyow-srikar-kavalis-projects.vercel.app",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 scraper = MangapillScraper()
 
 @app.get("/search")
